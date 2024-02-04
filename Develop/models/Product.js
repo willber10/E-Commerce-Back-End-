111ecf319cd577d74e0beb0d75664c87b20a9d00
willber10 <willberschickler@gamil.com>
2024-02-04 14:52:05 -0800

    Finished the modules section

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // price column
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    // stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    // category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
